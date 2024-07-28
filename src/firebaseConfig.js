35c5d7b62280ff64ecaf1c5c5a1402551073e07b
Farhad Afridi <afridifarhad149@gmail.com>
2024-07-28 05:08:49 +0500

    add env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-a0wGLrmOiV-1-pi7dCVCyx53XITA2qA",
-  authDomain: "student-management-syste-782ff.firebaseapp.com",
-  projectId: "student-management-syste-782ff",
-  storageBucket: "student-management-syste-782ff.appspot.com",
-  messagingSenderId: "121712036523",
-  appId: "1:121712036523:web:4e8fc4a740b3906292c5ee"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
